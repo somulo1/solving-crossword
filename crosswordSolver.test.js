de78ff30a8e03c0c907350b839b10fbdcd5129d4
John <johneliud4@gmail.com>
2024-10-05 18:14:29 +0300

    Update: Tests for an empty puzzleString and a puzzleString with newline characters.

--- crosswordSolver.test.js
+++ crosswordSolver.test.js
@@ -1,2 +1,29 @@
 import { expect, test } from 'vitest';
 import { crosswordSolver } from './crosswordSolver.js';
+import { parsePuzzle } from './crosswordSolver.js';
+
+test('checks if puzzleString is empty and return empty string', () => {
+  const puzzleString = '';
+  const expectedOutput = '';
+  const result = parsePuzzle(puzzleString);
+  expect(result).toEqual(expectedOutput);
+});
+
+test('converts puzzleString into 2D array', () => {
+  const puzzleString = 'AB\nCD';
+  const expectedOutput = [
+    ['A', 'B'],
+    ['C', 'D'],
+  ];
+  const result = parsePuzzle(puzzleString);
+  expect(result).toEqual(expectedOutput);
+});
+
+/*
+test('returns 3 empty arrays if puzzleString is \n\n\n', () => {
+  const puzzleString = '\n\n\n';
+  const expectedOutput = [[], [], []];
+  const result = parsePuzzle(puzzleString);
+  expect(result).toEqual(expectedOutput);
+});
+*/
