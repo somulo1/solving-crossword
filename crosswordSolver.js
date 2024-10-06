4caf8482b7d3c12638d238b1c56d82a019e17ae2
somulo1 <samuelokothomulo37@gmail.com>
2024-10-06 18:35:25 +0300

    chore: adentify the potential starting point

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -53,3 +53,41 @@ words.sort((a, b) => b.length - a.length);
 const shortest = words[words.length-1].length;
 const wordStarts = [];
 let tag = 0; // To track the number of placed words
+
+// Render 'emptyPuzzle into 2D array (array of array of string)
+const grid = emptyPuzzle.split('\n').map(line => line.split(''));
+const height = grid.length;
+const width = grid[0].length;
+
+  // Identify potential starting points for placement
+  for (let i = 0; i < height; i++) {
+      for (let j = 0; j < width; j++) {
+          if (grid[i][j] === '2') {
+              tag += 2;
+          } else if (grid[i][j] === '1') {
+              tag += 1;
+          }
+          if (grid[i][j] === '1' || grid[i][j] === '2') {
+              // Check horizontally
+              if (j === 0 || grid[i][j-1] === '0' || grid[i][j-1] === '.') {
+                  let k = j;
+                  while (k < width && (grid[i][k] === '0' || grid[i][k] === '1' || grid[i][k] === '2')) {
+                      k++;
+                  }
+                  if (k - j >= shortest) { 
+                      wordStarts.push({ row: i, col: j, direction: 'horizontal' });
+                  }
+              }
+              // Check vertically
+              if (i === 0 || grid[i-1][j] === '0' || grid[i-1][j] === '.') {
+                  let m = i;
+                  while (m < height && (grid[m][j] === '0' || grid[m][j] === '1' || grid[m][j] === '2')) {
+                      m++;
+                  }
+                  if (m - i >= shortest) { 
+                      wordStarts.push({ row: i, col: j, direction: 'vertical' });
+                  }
+              }
+          }
+      }
+  }
